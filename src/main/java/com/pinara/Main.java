748f8f50a709492ead65d3f9a8e79e6221372273
dpinara <dpinara@gmail.com>
2017-02-04 15:07:55 +0530

    Added Random logic

--- src/main/java/com/pinara/Main.java
+++ src/main/java/com/pinara/Main.java
@@ -1,8 +1,23 @@
 package com.pinara;
 
+/**
+ * This class is small but have buge potential. Watch out !!
+ */
 public class Main {
 
+    /**
+     * Enter the world..
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World! Good ");
+    }
+
+    public boolean findRightResult(){
+        if (false) {
+            return Math.random() > .5 ;
+        }
+
+        return false;
     }
 }
