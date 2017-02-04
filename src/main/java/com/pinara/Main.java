6571905bda781244a8d1d50b9fbd8a965507aa52
dpinara <dpinara@gmail.com>
2017-02-04 13:58:32 +0530

    Restructuring Class with Maven project

---
+++ src/main/java/com/pinara/Main.java
@@ -0,0 +1,10 @@
+package com.pinara;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Good ");
+        System.out.println("Hello World! One more time ");
+        System.out.println("Hello World! Second time ");
+    }
+}
