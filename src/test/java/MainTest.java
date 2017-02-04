df2ecfee5ebb18c7bf42f8e839e70503ad5ebb7b
dpinara <dpinara@gmail.com>
2017-02-04 15:26:00 +0530

    commiting test and pitest dependency

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by Admin on 04-02-2017.
  */
 public class MainTest {
     public static void main(String[] args) {
         System.out.printf("Test Will Go here..");
+    }
+
+    @Test
+    public void testSomethingInitial(){
+        Assert.assertTrue(true);
     }
 }
