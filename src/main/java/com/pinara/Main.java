f4f66c262249f11d183c28c6e7af4fc3872b9c60
dpinara <dpinara@gmail.com>
2017-02-04 14:03:18 +0530

    Updating latest changes

--- src/main/java/com/pinara/Main.java
+++ src/main/java/com/pinara/Main.java
@@ -4,7 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! Good ");
-        System.out.println("Hello World! One more time ");
-        System.out.println("Hello World! Second time ");
     }
 }
